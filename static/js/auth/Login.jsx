fe1624899fa4b7ea0b4adf0a1751a9f65dd6096a
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-06-15 19:30:32 -0700

    Created Registration feature and began Login Feature

---
+++ static/js/auth/Login.jsx
@@ -0,0 +1,65 @@
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email:'',
+            password: ''
+        }
+
+    }
+    handleChange = (event) => {
+        this.setState({
+         [event.target.name]: event.target.value
+        })
+    }
+    handleSubmit = (event => {
+        event.preventDefault()
+        
+        const formData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        fetch('/api/login', {
+            method:'post',
+            body: JSON.stringify(formData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data === "Login") {
+              this.props.setLoggedIn(this.state.email)
+          } else {
+              alert("Invalid entry, please try again.")
+          }
+        })
+    })    
+    render() {
+        return(
+            <div>
+            <h3>Please Log In:</h3>
+            <br />
+                <form onSubmit = {this.handleSubmit}>
+                    <input 
+                        type = "email"
+                        name = "email"
+                        placeholder = "Email"
+                        value = {this.state.email}
+                        onChange = {this.handleChange}
+                        required
+                    />
+                    <br />
+                    <input 
+                        type = "password"
+                        name = "password"
+                        placeholder = "Password"
+                        value = {this.state.password}
+                        onChange = {this.handleChange}
+                        required
+                    />
+                    <br />
+                    <button type = "submit">Log In</button>
+                </form>
+            </div>
+        )
+    }
+}
