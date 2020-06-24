fc09aaa3cf93d42a00970986d057de752b350e0e
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-06-24 16:24:13 -0700

    created dropdown county search in Directory.jsx

--- static/js/directory.jsx
+++ static/js/directory.jsx
@@ -23,6 +23,9 @@ class MidwifePreview extends React.Component {
 class Directory extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			value: ''
+		}
 	}
 
 	componentDidMount() {
@@ -33,15 +36,45 @@ class Directory extends React.Component {
 		}
 		
 	}
-
+	handleChange = (event) => {
+		this.setState({
+			value: event.target.value
+		})
+	}
+	handleSubmit = (event) => {
+		event.preventDefault()
+		alert('You selected: ' + this.state.value)
+	}
 	render() {
 		const listMidwives = this.props.midwifeData.map((midwife) => 
 			<MidwifePreview midwife={midwife} key={midwife.mw_id} />);
 
 		return (
 			<div>
-				<h1>Directory of Midwives</h1>
-				<div>{listMidwives}</div>
+				<div>
+					<h1>Directory of Midwives</h1>
+				</div>
+				<div>	
+					<form onSubmit={this.handleSubmit}>
+						<label>
+							Filter by county
+							<br />
+							<select value={this.state.value} onChange={this.handleChange} >
+								<option value='' defaultValue>select county</option>
+								<option value='Alameda' >Alameda</option>
+								<option value='Contra Costa' >Contra Costa</option>
+								<option value='Marin' >Marin</option>
+								<option value='San Francisco' >San Francisco</option>
+								<option value='San Mateo' >San Mateo</option>
+								<option value='Solano' >Solano</option>
+							</select>
+						</label>
+						<input type="submit" value='Select' />
+					</form>
+				</div>	
+				<div>
+					{listMidwives}
+				</div>
 			</div>
 		);
 	}
