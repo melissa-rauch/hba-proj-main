67c412d107400fa35b6800805895131424937863
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-06-30 18:58:00 +0000

    changed NavBar text

--- static/js/components/NavBar.jsx
+++ static/js/components/NavBar.jsx
@@ -13,7 +13,7 @@ class NavBar extends React.Component{
                     <div className="navbar-nav" id="#navbarSupportedContent">
                         <ul className="navbar-nav">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/">My Profile<span className="sr-only">(current)</span></a>
+                                <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/directory">View Directory</a>
